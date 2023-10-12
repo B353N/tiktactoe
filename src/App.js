0343a42af203805f97e30e568831c8ddc8d4da96
RSlavov <r.slavov.work@gmail.com>
2023-10-12 11:03:23 +0300

    modified:   src/App.js 	modified:   src/index.css

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h1>Tic Tac Toe</h1>
       <div className='board'>
         <div className='square'></div>
         <div className='square'></div>
