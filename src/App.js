ca3b3685f59387eb4575decec7a8a21527c48ffe
RSlavov <r.slavov.work@gmail.com>
2023-10-12 11:12:31 +0300

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -2,23 +2,36 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Tic Tac Toe</h1>
-      <div className='board'>
-        <div className='square'></div>
-        <div className='square'></div>
-        <div className='square'></div>
-        <div className='square'></div>
-        <div className='square'></div>
-        <div className='square'></div>
-        <div className='square'></div>
-        <div className='square'></div>
-        <div className='square'></div>
+class App extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      player_turn: 'X',
+      board: ['', '', '', '', '', '', '', '', '']
+    }
+  }
+
+  squareClicked(index) {
+    let player_turn = this.state.player_turn
+    let board = this.state.board
+
+    board[index] = player_turn
+    player_turn = player_turn === 'X' ? 'O' : 'X';
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Tic Tac Toe</h1>
+        <div className='board'>
+          {this.state.board.map((square) => {
+            return <div onClick={this.squareClicked} className='square'>{square}</div>
+          })}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
